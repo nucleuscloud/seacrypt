59a93c3a44719077e9222216cb32c4005f5cd0bc
Nick Z <2420177+nickzelei@users.noreply.github.com>
2022-07-10 11:42:44 -0700

    exports env var key conversion logic

--- cmd/exec-env.go
+++ cmd/exec-env.go
@@ -128,7 +128,7 @@ func decrypt(ctx context.Context, kmsClient *kms.Client, filePath string, keyFil
 func getEnvWithSecrets(st map[string]string) ([]string, error) {
 	env := os.Environ()
 	for key, value := range st {
-		env = append(env, fmt.Sprintf("%s=%s", toScreamingSnake(key), value))
+		env = append(env, fmt.Sprintf("%s=%s", seacrypt.ToScreamingSnake(key), value))
 	}
 	return env, nil
 }
@@ -167,65 +167,4 @@ func getKey(path string, deletePrivateKeyAfterUse bool) (string, error) {
 		return "", fmt.Errorf("length of key must be greater than 0")
 	}
 	return strings.TrimSpace(stringContents), nil
-}
-
-// taken from: https://github.com/iancoleman/strcase/blob/a61ebb85b34d7b831590cd8fa7faafadc161a652/snake.go#L66
-// ToScreamingSnake converts a string to SCREAMING_SNAKE_CASE
-func toScreamingSnake(s string) string {
-	return toScreamingDelimited(s, '_', "", true)
-}
-
-// toScreamingDelimited converts a string to SCREAMING.DELIMITED.SNAKE.CASE
-// (in this case `delimiter = '.'; screaming = true`)
-// or delimited.snake.case
-// (in this case `delimiter = '.'; screaming = false`)
-func toScreamingDelimited(s string, delimiter uint8, ignore string, screaming bool) string {
-	s = strings.TrimSpace(s)
-	n := strings.Builder{}
-	n.Grow(len(s) + 2) // nominal 2 bytes of extra space for inserted delimiters
-	for i, v := range []byte(s) {
-		vIsCap := v >= 'A' && v <= 'Z'
-		vIsLow := v >= 'a' && v <= 'z'
-		if vIsLow && screaming {
-			v += 'A'
-			v -= 'a'
-		} else if vIsCap && !screaming {
-			v += 'a'
-			v -= 'A'
-		}
-
-		// treat acronyms as words, eg for JSONData -> JSON is a whole word
-		if i+1 < len(s) {
-			next := s[i+1]
-			vIsNum := v >= '0' && v <= '9'
-			nextIsCap := next >= 'A' && next <= 'Z'
-			nextIsLow := next >= 'a' && next <= 'z'
-			nextIsNum := next >= '0' && next <= '9'
-			// add underscore if next letter case type is changed
-			if (vIsCap && (nextIsLow || nextIsNum)) || (vIsLow && (nextIsCap || nextIsNum)) || (vIsNum && (nextIsCap || nextIsLow)) {
-				prevIgnore := ignore != "" && i > 0 && strings.ContainsAny(string(s[i-1]), ignore)
-				if !prevIgnore {
-					if vIsCap && nextIsLow {
-						if prevIsCap := i > 0 && s[i-1] >= 'A' && s[i-1] <= 'Z'; prevIsCap {
-							n.WriteByte(delimiter)
-						}
-					}
-					n.WriteByte(v)
-					if vIsLow || vIsNum || nextIsNum {
-						n.WriteByte(delimiter)
-					}
-					continue
-				}
-			}
-		}
-
-		if (v == ' ' || v == '_' || v == '-' || v == '.') && !strings.ContainsAny(string(v), ignore) {
-			// replace space/underscore/hyphen/dot with delimiter
-			n.WriteByte(delimiter)
-		} else {
-			n.WriteByte(v)
-		}
-	}
-
-	return n.String()
 }
